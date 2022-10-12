2ed743814f1eeb4e66802ed8493ca92e0ffb437d
RavenInDisguise <monicaalfarop@estudiantec.cr>
2022-10-12 11:06:00 -0600

    Cambio en el nombre del paquete

--- src/main/java/InterfazCreacionPersonajes/CreacionPersonajesGui.java
+++ src/main/java/InterfazCreacionPersonajes/CreacionPersonajesGui.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package disenno.creacionpersonajesgui;
+package InterfazCreacionPersonajes;
 import InterfazJuego.campoBatalla_Juego;
 import LogicaJuego.*;
 import java.util.ArrayList;
